7ecf4d68ad4a7dcd2999694698d25dc7e44ebaed
hezl1592 <hezl1592@163.com>
2019-03-31 21:23:25 +0800

    IPcamera multi-process

---
+++ Ipcamera/camera_multiprocess.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Time    : 2019/3/31 22:12
+# Author  : zlich
+# Filename: camera_multiprocessing_1.py
+import multiprocessing
+import cv2
+
+
+def cam_loop(queue, stopped):
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        print(queue.qsize())
+        ret, img = cap.read()
+        if ret:
+            queue.put(img)
+        print('stop value: ', stopped.value)
+        if stopped.value:
+            break
+    cap.release()
+    print('cam_loop stopped')
+
+
+if __name__ == '__main__':
+    stopped = multiprocessing.Value("l", 0)
+    read_Queue = multiprocessing.Queue(2)
+
+    cam_process = multiprocessing.Process(target=cam_loop, args=(read_Queue, stopped,))
+
+    cam_process.start()
+
+    while True:
+        frame = read_Queue.get()
+        cv2.imshow('pepe', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print('pause')
+            break
+        cv2.waitKey(20)
+    cam_process.terminate()
+    print("all done")
